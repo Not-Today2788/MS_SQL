a5e3a964f8cac37377f7b02568a777159ca2ccaf
Prakhar <prakhar2788@gmail.com>
2024-02-16 14:03:04 +0530

    [prakhar] Add: Implemented numeric function in MS SQL

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -41,3 +41,25 @@ Order BY
 
 select * from employee_info order by empSalary asc;
 select * from employee_info order by empSalary desc;
+
+Aggregate Function or Group Function
+
+select sum(empSalary) from employee_info; 
+select min(empSalary) from employee_info; 
+select max(empSalary) from employee_info; 
+select avg(empSalary) from employee_info; 
+select count(empSalary) from employee_info;
+
+Numeric Functions
+
+select abs(-5);
+
+select ceiling(77.2);
+
+select floor(77.8); 
+
+select sign(10) , sign(-10), sign(0);
+
+select square(5), sqrt(64), pi(), cos(30), tan(45), sin(15), cot(90));
+
+select exp(1);
