2da57fa36c9c432a25a92f70ecbbba3907ed46bd
Prakhar <prakhar2788@gmail.com>
2024-02-16 14:59:29 +0530

    [prakhar] Add: Created Database in MS SQL

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,17 +1,2 @@
-create table employee_info
-( 
-	empId integer primary key,
-	empName varchar(20) not null,
-	empSalary decimal(10,2) not null,
-	job varchar(20),
-	phone int unique,
-	deptId integer not null
-);
-
-
-create table department_info
-(
-	depId integer not null,
-	depName varchar(20) not null,
-	depLocation varchar(50)
-);
+CREATE DATABASE employeeDB; 
+USE employeeDB;
